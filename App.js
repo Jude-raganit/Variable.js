618b34ab2b2ec4c49c67f3032f9fbf7f6ca774d6
Jude-raganit <juderaganit56@gmail.com>
2024-09-09 07:34:30 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+let name = Jude Lawrence Raganit;
+console.log ('name');
+
+let whatdoyouwannabecomeinyourlife = programmer;
+console.log('whatdoyouwannabecomeinyourlife');
+
+let gender = male;
+console.log('gender');
+
+let Twitterhandle = @lwrence Jornacion;
+console.log('twitterhandle')
